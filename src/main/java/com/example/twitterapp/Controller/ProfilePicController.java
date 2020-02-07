5e4b193689a9898361abf7a5d183e38be3f8fc1a
S.Pragashraj <rajpragashraj30@gmail.com>
2020-02-07 20:10:47 +0530

    getProfilePic

--- src/main/java/com/example/twitterapp/Controller/ProfilePicController.java
+++ src/main/java/com/example/twitterapp/Controller/ProfilePicController.java
@@ -3,16 +3,13 @@ package com.example.twitterapp.Controller;
 import com.example.twitterapp.Model.ProfilePic;
 import com.example.twitterapp.Repository.ProfilePicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.File;
+import java.io.*;
 
 @RestController
 @RequestMapping("/api/")
@@ -30,7 +27,7 @@ public class ProfilePicController {
                 byte[] imageBytes = file.getBytes();
                 ByteArrayInputStream bis = new ByteArrayInputStream(imageBytes);
                 BufferedImage bImage2 = ImageIO.read(bis);
-                String path="C:\\Users\\Home\\Documents\\Twitter App\\PofilePics\\amal.jpg";
+                String path="C:\\Users\\Home\\Documents\\Twitter App\\PofilePics\\"+username+".jpg";
                 ImageIO.write(bImage2, "jpg", new File(path) );
                 ProfilePic profilePic=new ProfilePic(username,path);
                 profilePicRepository.save(profilePic);
@@ -39,5 +36,15 @@ public class ProfilePicController {
         {
             System.out.println(e.toString());
         }
+    }
+
+    @GetMapping(value="getPicture",produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] getPicture(@RequestParam("name") String username)throws IOException{
+        File file=new File("C:\\Users\\Home\\Documents\\Twitter App\\PofilePics\\"+username+".jpg");
+        byte[] bytesArray = new byte[(int) file.length()];
+        FileInputStream fis = new FileInputStream(file);
+        fis.read(bytesArray);
+        fis.close();
+        return bytesArray;
     }
 }
